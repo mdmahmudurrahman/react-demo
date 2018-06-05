36e9a8e1e0161d8860284579707e78add426c809
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-05 09:42:36 +0700

    Adding class component and inline style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>    
+    </div> 
+  )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
