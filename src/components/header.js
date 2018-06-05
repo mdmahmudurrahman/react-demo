d23ec3b5a90b2f50920a71de9c20c70fd7053929
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-05 14:59:24 +0700

    Using glamor package to write inline css efficiently

--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,9 @@ import React, { Component } from 'react';
 import '../css/styles.css';
 
 class Header extends Component {
-  // constructor(props){
-  //   super(props)
-
-  //   this.state = {
-  //     keywords: 'Hello'
-  //   }
-  // }
-
   state = {
     keywords: 'Hello'
   }
-
-  // inputChangeHandler(event) {
-  //   // console.log(event.target.value);
-  //   this.setState({
-  //     keywords: event.target.value
-  //   });
-  // }
 
   inputChangeHandler = (event) => {
     this.setState({
@@ -29,18 +14,8 @@ class Header extends Component {
   }
 
   render() { 
-    const style = {
-      background: 'red' 
-    }
-
-    if(this.state.keywords !== '') {
-      style.background = 'gray'
-    } else {
-      style.background = 'red'
-    }
-
     return (
-      <header className="header" style={style}>
+      <header className="header">
         <div className="logo">Logo</div>   
         <input 
           type="text" 
@@ -52,13 +27,3 @@ class Header extends Component {
 }
 
 export default Header;
-
-
-// Another way of calling
-{/* <input 
-  type="text" 
-  onChange={(e) => this.inputChangeHandler(e)}
-/> */}
-
-// By using es6 fat arrow function, we can omit the use of bind(this)
-// onChange={this.inputChangeHandler.bind(this)
--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { css } from 'glamor';
 
 const NewsListItem = (props) => {
+
+  let newsItem = css({
+    padding: '20px',
+    boxSizing: 'border-box',
+    borderBottom: '1px solid grey'
+  }) 
+
   return(
-    <div>
+    <div {...newsItem}>
       <h3>{props.newsItem.title}</h3>
       <p>{props.newsItem.feed}</p>
     </div>
   )
-}
+} 
 
 export default NewsListItem;
