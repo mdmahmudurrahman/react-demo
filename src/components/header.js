9554fbd213b6fd7a376abf8e3416bde27b2e873c
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-05 11:32:48 +0700

    State with class component

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,34 @@ import React, { Component } from 'react';
 import '../css/styles.css';
 
 class Header extends Component {
-  
-  inputChangeHandler(event) {
-    console.log(event.target.value);
+  // constructor(props){
+  //   super(props)
+
+  //   this.state = {
+  //     keywords: 'Hello'
+  //   }
+  // }
+
+  state = {
+    title: 'The keywords are:',
+    keywords: 'Hello'
+  }
+
+  // inputChangeHandler(event) {
+  //   // console.log(event.target.value);
+  //   this.setState({
+  //     keywords: event.target.value
+  //   });
+  // }
+
+  inputChangeHandler = (event) => {
+    this.setState({
+      keywords: event.target.value
+    });
   }
 
   render() { 
-
+    console.log(this.state.keywords);
     return (
       <header className="header">
         <div className="logo">Logo</div>   
@@ -17,6 +38,8 @@ class Header extends Component {
           type="text" 
           onChange={this.inputChangeHandler}
         />
+        <div>{this.state.title}</div>
+        <div>{this.state.keywords}</div>
       </header>
     )
   }  
@@ -30,3 +53,6 @@ export default Header;
   type="text" 
   onChange={(e) => this.inputChangeHandler(e)}
 /> */}
+
+// By using es6 fat arrow function, we can omit the use of bind(this)
+// onChange={this.inputChangeHandler.bind(this)
