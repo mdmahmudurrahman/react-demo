88ea5bc08e52e23286cc60fca8e24c7da4938d60
md.mahmudur.rahman <md.mahmudur.rahman.bd@gmail.com>
2018-06-05 10:36:28 +0700

    Calling React event

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,30 @@ import React, { Component } from 'react';
 import '../css/styles.css';
 
 class Header extends Component {
+  
+  inputChangeHandler(event) {
+    console.log(event.target.value);
+  }
+
   render() { 
 
     return (
       <header className="header">
         <div className="logo">Logo</div>   
-        <input type="text" />
+        <input 
+          type="text" 
+          onChange={this.inputChangeHandler}
+        />
       </header>
     )
   }  
 }
 
 export default Header;
+
+
+// Another way of calling
+{/* <input 
+  type="text" 
+  onChange={(e) => this.inputChangeHandler(e)}
+/> */}
